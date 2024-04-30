e419625e380ab4ccd7cfbb68240ef83adcf2b2ec
Miraclev1 <127039117+Miracleprjkt@users.noreply.github.com>
2024-04-30 17:25:58 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,17 +1,10 @@
-#
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 TFILE=$PWD/out/hapticspath.patched
 [ ! -d "out" ]&& mkdir -p out
 RET=0
 REVERSE=0
 
 cd bootable/recovery
-git apply --reverse --check ../../device/itel/S666LN/patches/0001-Change-haptics-activation-file-path.patch || REVERSE=$?
+git apply --reverse --check ../../device/itel/S66LN/patches/0001-Change-haptics-activation-file-path.patch || REVERSE=$?
 cd ../../
 
 if [ -f "$TFILE" ];then
@@ -20,7 +13,7 @@ elif [ $REVERSE -eq 0 ]; then
     echo "$TFILE is not found but git is able to reverse haptics path patch, assuming it's already patched, skipping"
 else
     cd bootable/recovery
-    git apply ../../device/itel/S666LN/patches/0001-Change-haptics-activation-file-path.patch || RET=$?
+    git apply ../../device/itel/S66LN/patches/0001-Change-haptics-activation-file-path.patch || RET=$?
     cd ../../
     if [ $RET -ne 0 ];then
         echo "ERROR: minuitwrp/events.cpp could not be patched! Vibration in TWRP will not work."
